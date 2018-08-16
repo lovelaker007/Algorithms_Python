6cd69e45b6f88b0e3a3cee91edc4bc7275020ddf
lovelaker007 <lovelaker7@gmail.com>
2018-08-16 17:02:02 +0800

    numbers of1

---
+++ find_max_subarray.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+
+'''
+输入一个整形数组，数组里有正数也有负数。组中连续的一个或多个整数组成一个子数组，
+每个子数组都有一个和。求所有子数组的和的最大值
+'''
+
+def find_max_subarray(array):
+    if not array:
+        return None
+    cur_max = 0
+    ori_max = -0xffffffff
+    for i in range(len(array)):
+        if cur_max <= 0:
+            cur_max = l[i]
+        else:
+            cur_max += l[i]
+
+        if cur_max > ori_max:
+            ori_max = cur_max
+    return ori_max
+
+def find_max_subarray2(l):
+    if not l:
+        return None
+    old_start = start = end = 0
+    cur_max = 0
+    ori_max = -0xffffffff
+    
+    for i in range(len(l)):
+        if cur_max <= 0:
+            cur_max = l[i]
+            start = i
+        else:
+            cur_max += l[i]
+
+        if cur_max > ori_max:
+            ori_max = cur_max
+            old_start = start
+            end = i
+        # print old_start, end
+
+    r = []
+    for i in range(old_start, end+1):
+        r.append(l[i])
+    print 'max subarray is: %s\nsum is %d\n' % (r, ori_max) 
+
+
+if __name__ == '__main__':
+    l = [1, -2, 3, 10, -4, 7, 2, -5]
+    print find_max_subarray(l)
+    find_max_subarray2(l)
+    l = [-10, -5, -7, -12, -1, -9, -7]
+    print find_max_subarray(l)
+    find_max_subarray2(l)
+    l = list(range(15))
+    print find_max_subarray(l)
+    find_max_subarray2(l)
+
+
---
+++ numbers_of1_between1_andn.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+
+'''
+输入一个整数n，求从1到n这n个整数的十进制表示中1出现的次数。
+例如输入12，从1到12这些整数中包含1的数字有1,10,11和12，1一共出现了5次
+'''
+
+'''
+一位数：1 0-9
+二位数：9*1+10+1 = 20 0-99
+三位数：9*20+100+20 = 300 0-999
+四位数：9*300+1000+300 = 4000
+'''
+
+# 暴力解法
+def func1(x):
+    num = 0
+    for i in range(x+1):
+        m = str(i)
+        for n in m:
+            if n == '1':
+                num += 1
+    return num
+
+# 打印数字的每一位
+def func2(num):
+    while num:
+        print num % 10
+        num /= 10
+
+# 任意两位数
+def func3(num):
+    shi = num/10
+    ge = num%10
+    if ge == 0:
+        num_ge = shi*1  
+    else:
+        num_ge = shi*1 + 1
+    if shi > 1:
+        num_shi = 10
+    else:
+        num_shi = ge+1
+    return num_ge + num_shi
+
+'''
+和书中的解法不一样，每次循环统计一位上1出现的次数
+对于任意的整数 4427689，假设统计千位上1出现的次数
+    整数n从1逐渐增加的过程中，千位上的数值在0-9之间循环变化(n小于1000时，将千位看作0)
+    想象一下千位上的数值：1000个0，1000个1，1000个2 ... 1000个9，然后循环
+    循环的次数是千位的高位组成的数值，本示例中是442。那么在442次循环中，1出现的次数是1000*442
+
+    千位上的数值完成442次完整的循环后，还会从0变化到7
+    如果千位上的数字是0，则还没有出现1
+    如果是1，则1出现的次数不足1000，应该为千位的低位组成的数值，本示例中是689
+    如果大于1，1出现的次数都是1000次
+
+    整个序列中，千位上出现1的次数是 689+1000*42
+'''
+
+def func5(num):
+    r = 0
+    weishu = 0
+    houmian = 0
+    while num:
+        # 当前位的数值
+        m = num%10
+        # 当前位前面的数值，表示当前位从0-9循环的次数
+        num = num/10
+        # 已经处理的位数
+        weishu += 1
+        # 所有完成的循环中，当前位为1的次数
+        r_xunhuan = num*(10**(weishu-1))
+        # 计算未完成的循环，当前位为1的次数
+        if m == 0:
+            r_banxunhuan = 0
+        elif m == 1:
+            r_banxunhuan = houmian+1
+        else:
+            r_banxunhuan = 10**(weishu-1)
+        # print m, r_xunhuan, r_banxunhuan
+        r += (r_xunhuan+r_banxunhuan)
+        houmian = 10**(weishu-1)*m + houmian
+    return r
+
+
+            
+if __name__ == '__main__':
+    l = [50, 667, 1000, 8994, 10000, 999]
+    for i in l:
+        print '%d\t%d' % (func1(i), func5(i))
+
